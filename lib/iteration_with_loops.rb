bf7f72bcc959c285f87c665d7d9476f96f78d2f7
Music Meier <musicm987@gmail.com>
2020-08-13 23:37:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,10 @@ def join_nested_strings(src)
     element_index = 0 
     while element_index < src[row_index].count do
       if src[row_index][element_index].class == String
-      just_string << src[row_index][element_index]
+      just_string << src[row_index][element_index] + " "
        
     end
-     element_index += 1
+     element_index += 1 
   end
   row_index += 1
 end
